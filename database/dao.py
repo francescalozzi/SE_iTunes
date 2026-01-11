3acac481295b72ea626fba15a5cc9bb2f012c3a5
francescalozzi <f.lozzi0405@gmail.com>
2026-01-11 08:54:17 +0100

    sviluppo

--- database/dao.py
+++ database/dao.py
@@ -1,19 +1,50 @@
 from database.DB_connect import DBConnect
+from model.album import Album
+
 
 class DAO:
     @staticmethod
-    def query_esempio():
+    def get_all_album_by_min_durata(min_duration):
         conn = DBConnect.get_connection()
-
         result = []
+        cursor = conn.cursor(dictionary=True)
+
+        query = """SELECT a.id, a.title, a.artist_id, SUM(t.milliseconds) / 60000 AS duration
+                    FROM album a, track t
+                    WHERE a.id = t.album_id
+                    GROUP BY a.id, a.title, a.artist_id
+                    HAVING duration >= %s"""
+
+        cursor.execute(query, (min_duration, ))
+        for row in cursor:
+            album = Album(id=row['id'], title=row['title'], artist_id=row['artist_id'], duration=row['duration'])
+            result.append(album)
+
+        cursor.close()
+        conn.close()
+        return result
+
+
+    @staticmethod
+    def get_albums_playlist_map(albums):
+        conn = DBConnect.get_connection()
+        result = {a: set() for a in albums}
+        albums_ids = tuple(a.id for a in albums)
+
+        if not albums_ids:
+            return result
 
         cursor = conn.cursor(dictionary=True)
-        query = """ SELECT * FROM esempio """
+        query = f"""
+                    SELECT t.album_id, pt.playlist_id
+                    FROM track t, playlist_track pt
+                    WHERE t.id = pt.track_id AND t.album_id in {albums_ids}"""
 
         cursor.execute(query)
-
         for row in cursor:
-            result.append(row)
+            album = next((a for a in albums if a.id == row['album_id']), None)
+            if album:
+                result[album].add(row['playlist_id'])
 
         cursor.close()
         conn.close()
--- model/model.py
+++ model/model.py
@@ -1,2 +1,54 @@
+import networkx as nx
+from database.dao import DAO
+
+
 class Model:
-    pass
+    def __init__(self):
+        self.G = nx.Graph()
+        self.albums = []
+        self.album_playlist_map = {}
+        self.soluzione_best = []
+        self.id_map = {}
+
+    def carica_albums(self,min_duration):
+        self.albums = DAO.get_all_album_by_min_durata(min_duration)
+        self.id_map = {a.id: a for a in self.albums}  #ASSOCIA AD OGNI ID L'OGGETTO ALBUM
+
+    def carica_album_playlists(self):
+        self.album_playlist_map = DAO.get_albums_playlist_map(self.albums)
+
+    def build_graph(self):
+        self.G.clear()
+        self.G.add_nodes_from(self.albums)
+
+        for i, a1 in enumerate(self.albums):
+            for a2 in self.albums[i+1:]:
+                if self.album_playlist_map[a1] & self.album_playlist_map[a2]:
+                    self.G.add_edge(a1, a2)
+
+
+    def get_component(self,album):
+        if album not in self.G:
+            return []
+        return list(nx.node_connected_component(self.G, album))
+
+
+    def compute_best_set(self,start_album, max_duration):
+        component = self.get_component(start_album)
+        self.soluzione_best = []
+        self._ricorsione(component, [start_album], start_album.duration ,max_duration)
+        return self.soluzione_best
+
+    def _ricorsione(self, albums, current_set, current_duration, max_duration):
+        if len(current_set) > len(self.soluzione_best):
+            self.soluzione_best = current_set[:]
+
+        for album in albums:
+            if album in current_set:
+                continue
+            new_duration = current_duration + album.duration
+
+            if new_duration <= max_duration:
+                current_set.append(album)
+                self._ricorsione(albums, current_set, new_duration, max_duration)
+                current_set.pop()
